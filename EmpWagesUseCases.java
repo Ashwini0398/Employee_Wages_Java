d522c578ddbda31ffa7606bbe33b1458204d1373
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-01-22 16:44:17 +0530

    MODIFIED:add part time and full time employee wage

--- EmpWagesUseCases.java
+++ EmpWagesUseCases.java
@@ -4,6 +4,7 @@ public class EmpWagesUseCases
 	{
 		//constants
 		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;
 		int EMP_RATE_PER_HR =20;
 
 		//variable
@@ -12,7 +13,9 @@ public class EmpWagesUseCases
 
 		//computation of employee attendance
 		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if(empCheck == IS_FULL_TIME)
+		if(empCheck == IS_PART_TIME)
+			empHrs = 4;
+		else if(empCheck == IS_FULL_TIME)
 			empHrs=8;
 		else
 			empHrs=0;
