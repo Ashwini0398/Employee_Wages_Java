8f75615aa552965c036b2ccfaaae8cdabf6a4bee
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-01-22 17:00:05 +0530

    MODIFIED:Calculate Employee wages for month

--- EmpWagesUseCases.java
+++ EmpWagesUseCases.java
@@ -3,6 +3,7 @@ public class EmpWagesUseCases
 	public static final int IS_PART_TIME = 1;
         public static final int IS_FULL_TIME = 2;
         public static final int EMP_RATE_PER_HR =20;
+	 public static final int NUMBER_OF_WORKING_DAYS = 2;
 
 
 	public static void main (String args[])
@@ -11,24 +12,30 @@ public class EmpWagesUseCases
 		//variable
 		int empHrs=0;
 		int empWage=0;
+		int totalempwage = 0;
 
 		//computation of employee attendance
-		int empCheck =(int) Math.floor(Math.random() * 10) % 2;
-		switch((empCheck))
+		for ( int day=0; day<NUMBER_OF_WORKING_DAYS; day++ )
 		{
-			case IS_PART_TIME:
-				empHrs = 4;
-				break;
+			int empCheck =(int) Math.floor(Math.random() * 10) % 2;
+			switch((empCheck))
+			{
+				case IS_PART_TIME:
+					empHrs = 4;
+					break;
 
-			case IS_FULL_TIME:
-				empHrs =  8;
-				break;
+				case IS_FULL_TIME:
+					empHrs =  8;
+					break;
 
-			default:
-				empHrs = 0;
+				default:
+					empHrs = 0;
+			}
+
+			empWage= empHrs * EMP_RATE_PER_HR;
+			totalempwage += empWage;
+			System.out.println("Employee Wages: "+empWage);
 		}
-
-		empWage= empHrs * EMP_RATE_PER_HR;
-		System.out.println("Employee Wages: "+empWage);
+		System.out.println("Total employee wage  month is: " +totalempwage);
 	}
 }
