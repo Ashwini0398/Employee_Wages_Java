a67ed3d152bdd7416bc5304e5f855092a5242512
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-01-22 16:29:19 +0530

    ADDED: Employee Attendance

--- EmpWagesUseCases.java
+++ EmpWagesUseCases.java
@@ -5,7 +5,7 @@ public class EmpWagesUseCases
 		//constants
 		int IS_FULL_TIME = 1;
 
-		//computation
+		//computation of employee attendance
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if(empCheck == IS_FULL_TIME)
 			System.out.println("Employee is present");
