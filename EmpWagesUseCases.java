8568b0a4f3b2bc118fa5c75ec534445044606cad
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-01-22 16:52:51 +0530

    MODIFIED: Employee Wage using

--- EmpWagesUseCases.java
+++ EmpWagesUseCases.java
@@ -1,24 +1,33 @@
 public class EmpWagesUseCases
 {
+	public static final int IS_PART_TIME = 1;
+        public static final int IS_FULL_TIME = 2;
+        public static final int EMP_RATE_PER_HR =20;
+
+
 	public static void main (String args[])
 	{
-		//constants
-		int IS_FULL_TIME = 1;
-		int IS_PART_TIME = 2;
-		int EMP_RATE_PER_HR =20;
-
+	
 		//variable
 		int empHrs=0;
 		int empWage=0;
 
 		//computation of employee attendance
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if(empCheck == IS_PART_TIME)
-			empHrs = 4;
-		else if(empCheck == IS_FULL_TIME)
-			empHrs=8;
-		else
-			empHrs=0;
+		int empCheck =(int) Math.floor(Math.random() * 10) % 2;
+		switch((empCheck))
+		{
+			case IS_PART_TIME:
+				empHrs = 4;
+				break;
+
+			case IS_FULL_TIME:
+				empHrs =  8;
+				break;
+
+			default:
+				empHrs = 0;
+		}
+
 		empWage= empHrs * EMP_RATE_PER_HR;
 		System.out.println("Employee Wages: "+empWage);
 
